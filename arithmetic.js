f129c4fb7e93e56708262bad808aab1f63a2e084
malathiganesh <107910492+malathiganesh@users.noreply.github.com>
2022-09-10 17:10:09 +0530

    Arithmetic calculator and other operator

---
+++ arithmetic.js
@@ -0,0 +1,31 @@
+function addition(){
+    a=Number(document.arithmetic.first.value);
+    b=Number(document.arithmetic.second.value);
+    c=a+b;
+    document.arithmetic.total.value=c;
+  }
+  function subtract(){
+    a=Number(document.arithmetic.first.value);
+    b=Number(document.arithmetic.second.value);
+    c=a-b;
+    document.arithmetic.total.value=c;
+  }
+  function multiply(){
+    a=Number(document.arithmetic.first.value);
+    b=Number(document.arithmetic.second.value);
+    c=a*b;
+    document.arithmetic.total.value=c;
+  }
+  function divition(){
+    a=Number(document.arithmetic.first.value);
+    b=Number(document.arithmetic.second.value);
+    c=a/b;
+    document.arithmetic.total.value=c;
+  }
+  function modulo(){
+    a=Number(document.arithmetic.first.value);
+    b=Number(document.arithmetic.second.value);
+    c=a%b;
+    document.arithmetic.total.value=c;
+  }
+
---
+++ operator.js
@@ -0,0 +1,30 @@
+var a=2;
+a++;
+// a--;
+console.log(a);
+
+var b=4;
+b+=5;
+b=b+5;
+console.log(b);
+
+var c=2;
+console.log('Welome'+' '+c);
+
+var d=5 ;
+var e=10;
+if(d<e){
+    console.log('true');
+}
+else{
+    console.log('false');
+}
+
+if(d<=e && d<e){
+    console.log('true');
+}
+else{
+    console.log('false');
+}
+
+console.log(2<3 ? true : false);
